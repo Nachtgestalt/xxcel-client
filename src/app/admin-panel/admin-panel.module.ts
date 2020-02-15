27f127dc6a00c8b5a99f7771019eaeb774c0f903
Luis Angel <luis.angel.osorio@outlook.com>
2020-02-14 20:47:43 -0600

    Admin Panel started

--- src/app/admin-panel/admin-panel.module.ts
+++ src/app/admin-panel/admin-panel.module.ts
@@ -1,15 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import {ADMIN_PANEL_ROUTES} from './admin-panel.routes';
-import { LayoutModule } from '@angular/cdk/layout';
+import {LayoutModule} from '@angular/cdk/layout';
 import {AdminPanelComponent} from './admin-panel.component';
 import {AngularMaterialModule} from '../angular-material.module';
-
+import {ProductsComponent} from './pages/products/products.component';
+import {FooterComponent} from '../shared/footer/footer.component';
 
 
 @NgModule({
-  declarations: [DashboardComponent, AdminPanelComponent],
+  declarations: [
+    DashboardComponent,
+    AdminPanelComponent,
+    ProductsComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     ADMIN_PANEL_ROUTES,
@@ -17,4 +23,5 @@ import {AngularMaterialModule} from '../angular-material.module';
     AngularMaterialModule
   ]
 })
-export class AdminPanelModule { }
+export class AdminPanelModule {
+}
--- src/app/admin-panel/pages/admin-panel-pages.routes.ts
+++ src/app/admin-panel/pages/admin-panel-pages.routes.ts
@@ -1,7 +1,9 @@
 import {Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {ProductsComponent} from './products/products.component';
 
 export const ADMIN_PANEL_PAGES_ROUTES: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent, data: {title: 'Inicio'}},
+  {path: 'products', component: ProductsComponent, data: {title: 'Productos'}}
 ];
